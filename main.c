82ab975e4a99c0e785d3a52900db7754c6eba890
Taiki Tomita <g.tomita.t@gmail.com>
2019-06-23 19:05:46 +0900

    コンテキストを増やして、mainを中心にタスクスイッチするようにした.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include<signal.h>
 #include<sys/time.h>
 
-struct context *Cmain,*Ctest;
+struct context *Cmain,*Ctest1,*Ctest2;
 
 struct context* mkcontext(void (*func)(),void* stack,int stacksize){
 	struct context* c;
@@ -23,21 +23,33 @@ void test(){
 	int i;
 	double A=0.12345678;
 	for(i=0;1;i++){
-		printf("test:%d %lf\n",i,A);
+		printf("test1:%d %lf\n",i,A);
 		A=A*A;
-		swtch(&Ctest,Cmain);
+		swtch(&Ctest1,Cmain);
+	}
+}
+
+void test2(){
+	int i;
+	double A=0.12345678;
+	for(i=0;1;i++){
+		printf("test2:%d %lf\n",i,A);
+		A=A*A;
+		swtch(&Ctest2,Cmain);
 	}
 }
 
 int main(){
 	int i;
-	char stack[0x1000];
+	char stack1[0x1000];
+	char stack2[0x1000];
 
-	Ctest  = mkcontext(test,stack,0x1000);
+	Ctest1  = mkcontext(test,stack1,0x1000);
+	Ctest2  = mkcontext(test2,stack2,0x1000);
 
-	for(i=0;1;i++){
-		printf("main:%d\n",i);
-		swtch(&Cmain,Ctest);
+	while(1){
+		swtch(&Cmain,Ctest1);
+		swtch(&Cmain,Ctest2);
 	}
 	return 0;
 }
